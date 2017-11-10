469a8a6e857e033d024468c4f82d737d329fcd43
Kyu Taek Hwang <kyutaekhwang@gmail.com>
2017-11-10 15:28:14 -0500

    Editing main.js and Removed video.js
    Currently editing main.js, but code disabled until I can revisit it
    again.

--- script/main.js
+++ script/main.js
@@ -23,4 +23,52 @@ $(document).ready(function(){
 
 	// add paralax (stellar.js)
 	$.stellar();
+
+    //  $('footer').scrollToFixed( {
+    //     bottom: 0,
+    //     limit: $('footer').offset().top,
+    //     preFixed: function() { $(this).find('.footer-distributed'); },
+    //     postFixed: function() { $(this).find('.footer-distributed'); }
+    // });
+
+
+	// $('footer').hide();
+	// 	if($(document).height() < $(window).height()){
+	// 		$('footer').show();
+	// 	}
+	// $(window).resize(function(){
+	// 	console.log("resized");
+	// 	if($(document).height() > $(window).height()){
+	// 		console.log("hide footer now");
+	// 		$('footer').slideUp('slow');
+	// 	}
+	// 	else{
+	// 		$('footer').slideDown('slow');
+	// 	}
+	// });
+
+	// $(window).scroll(function(){        
+ //    	if ($(window).scrollTop() + $(window).height() >= $(document).height() - 0){
+ //            $('footer').slideDown('fast');
+ //            $('#white2').stop().animate({
+ //                bottom:'6px'
+ //            },400);
+ //   		}
+ //    	else{
+ //            $('footer').slideUp('fast');
+ //            $('#white2').stop().animate({
+ //                bottom:'-44px'
+ //        	},400);
+ //   		}
+	// });
+
+	// $(document).ready(function() {
+	// 	if ($(window).height() >= $(document).height() ){
+	// 		$('footer').data('size','hide');
+	// 	}
+	// 	else{
+	// 		$('footer').data('size','show');
+	// 	}
+	// });
+
 });
